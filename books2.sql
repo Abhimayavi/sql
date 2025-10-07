4ba0fab879a06a87ba33c2137e408179e8d213c6
Abhimayavi <abhijithj401@gmail.com>
2025-10-07 12:35:55 +0530

    Add initial SQL script for books and borrowers tables

---
+++ books2.sql
@@ -0,0 +1,13 @@
+INSERT INTO `bookss`(`book_id`, `title`) VALUES
+(1,'The Alchemist'),
+(2,'The Power of Now'),
+(3,' Think and Grow Rich'),
+(4,' Clean Code');
+ -- table bookss
+
+ INSERT INTO `borrowers`(`borrower_id`, `name`, `book_id`) VALUES 
+(101,'Alice ',1),
+(102,'Bob',2),
+(103,'Charlie',null);
+
+-- 
